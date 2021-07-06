68517384140e4b023e16d92bc566b737ce06011a
Nitin-U <66345351+Nitin-U@users.noreply.github.com>
2021-07-06 22:23:34 +0545

    commit

---
+++ website/script/script1.js
@@ -0,0 +1,8 @@
+/*-----------------------------js query for typeset--------------------------*/
+
+var typed = new Typed(".typing", {
+  strings: ["Website Developer", "Programmer", "Database Designer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
